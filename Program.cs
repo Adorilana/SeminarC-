75c7c06918b1654e2ada0d6728a43005e80ec4b4
Anita <79185734225@yandex.ru>
2022-08-14 17:34:27 +0300

    add zadacha 6

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@
 a = 5; b = 7 -> max = 7
 a = 2 b = 10 -> max = 10
 a = -9 b = -3 -> max = -3
-*/
+
 
 Console.Write("Введите число №1: ");
 int firstNumber = Convert.ToInt32(Console.ReadLine());
@@ -21,3 +21,25 @@ else
 {
 Console.WriteLine(secondNumber);
 }
+
+Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+4 -> да
+-3 -> нет
+7 -> нет
+*/
+
+Console.Write("Введите число №1: ");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+if (firstNumber %2==0) 
+{
+Console.WriteLine(firstNumber + " является четным ");
+
+}
+
+else 
+{
+Console.WriteLine(firstNumber + " НЕ является четным ");
+}
+
