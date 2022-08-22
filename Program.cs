12c0e07ad05e3c5111309a05145921a67ae76fa3
Anita <79185734225@yandex.ru>
2022-08-22 20:14:48 +0300

    add homework 2

--- Program.cs
+++ Program.cs
@@ -1,36 +1,57 @@
-﻿/* Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+﻿/* 
+19Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
 
-
-int number = new Random().Next(10,1000);
-
-int result1 = number / 10;
-int result2 = result1 % 10;
-int result3 = result2 % 10
-
-Console.WriteLine($"Вторая цифра числа {number} - это {result2}");
-
-
-Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
-
-
+Console.Write("Введите пятизначное число ");
 string number = Console.ReadLine();
 
-Console.WriteLine(number[2]);
-
-Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
-
+if (number[0] == number[4] && number[1] == number[3])
+{
+    Console.Write("число является палиндромом");
+} 
+else
+{
+    Console.Write("число НЕ является палиндромом");
+}
 */
 
+
+//21 Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+
+Console.Write("Введите координаты x1: ");
+int x1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите координаты x2: ");
+int x2 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите координаты y1: ");
+int y1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите координаты y2: ");
+int y2 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите координаты z1: ");
+int z1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите координаты z2: ");
+int z2 = Convert.ToInt32(Console.ReadLine());
+
+double distance = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(z2 - z1, 2));
+
+Console.WriteLine($"Расстояние между двумя точками в пространстве равно {distance}");
+
+
+/*
+Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+*/
+/*
 Console.Write("Введите число: ");
 int number = Convert.ToInt32(Console.ReadLine());
-
-if (number == 6 || number == 7) 
+int start = 1;
+while (start <= number)
 {
-Console.WriteLine("да");
-
+    int result = start * start * start;
+    Console.Write(" " + result);
+    start++;
 }
-
-else 
-{
-Console.WriteLine("нет");
-}
+*/
